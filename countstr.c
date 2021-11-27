44e1e5772acdf04721ee321449d2f0ffbb86e73e
Riwaj Ropakheti <45209233+RiwajRopakheti@users.noreply.github.com>
2021-11-27 16:04:16 +0545

    Add files via upload

---
+++ countstr.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include <string.h>
+#include <conio.h> 
+int main()
+{
+	char s[1000],c;  
+	int i, count=0;
+ 	printf("Enter  the string : ");
+    	gets(s);
+    	printf("Enter character to be searched: ");
+    	c=getchar();
+    	for(i=0;s[i];i++)  
+    	{
+    		if(s[i]==c && s[i]!=" ")
+    		{
+          		count++;
+		}		
+ 	}
+     	printf("character '%c' occurs %d times \n ",c,count);
+	return 0;
+}
